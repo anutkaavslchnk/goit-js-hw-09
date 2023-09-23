b0a239b26b7f49dd72f62413f1dcb518fd620689
anutkaavslchnk <131483541+anutkaavslchnk@users.noreply.github.com>
2023-09-23 19:07:55 +0000

    Deploying to gh-pages from @ anutkaavslchnk/goit-js-hw-09@7f071e5d471457ab9107261160f2a51b8d4aaf55 🚀

---
+++ 01-color-switcher.76596fee.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");let a;e.addEventListener("click",(()=>{a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,e.disabled=!0,t.disabled=!1}),1e3)}));t.addEventListener("click",(()=>{clearInterval(a),t.disabled=!0,e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.76596fee.js.map
