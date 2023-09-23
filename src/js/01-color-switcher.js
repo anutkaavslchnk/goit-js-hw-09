b64e9efe088d19cced0617ef2423cd3d4979da97
anutkaavslchnk <anutochkaaaaa14@gmail.com>
2023-09-23 21:59:58 +0300

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+const StartBtn=document.querySelector("[data-start]");
+const StopBtn=document.querySelector("[data-stop]");
+const body=document.querySelector("body");
+const typeStart=()=>{
+     timerStart=setInterval(()=>{
+        body.style.backgroundColor=getRandomHexColor();
+        StartBtn.disabled = true;
+        StopBtn.disabled = false;
+    }, 1000)
+    
+};
+StartBtn.addEventListener("click", typeStart);
+const typeStop=()=>{
+    clearInterval(timerStart);
 
+        StopBtn.disabled = true;
+        StartBtn.disabled = false;
+    
+};
+StopBtn.addEventListener("click", typeStop);
